ebb42622b377fb47b9cf51462779a58568e75169
sudheesh-tech-afk <gsudheeshr@gmail.com>
2026-01-07 17:37:49 +0100

    Add Entity class with basic attributes and methods

--- src/base_entity.py
+++ src/base_entity.py
@@ -1,1 +1,16 @@
+import pygame
+
+class Entity:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        pass
+
 
